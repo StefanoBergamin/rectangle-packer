f99f03ff81e46cfb5932069af61c63d9dcfa1703
LAPTOP-LQHESKE6\stefa <stefano.bergamin.001@gmail.com>
2025-01-12 13:28:24 +0100

    Project build

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // esbuild: {
-  //   target: 'esnext',
-  //   platform: 'linux',
-  // },
   plugins: [
     vue(),
     vueDevTools(),
