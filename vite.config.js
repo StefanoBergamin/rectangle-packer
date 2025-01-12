6f891fe22edf7e044efa82de85f4aa4b808c2948
LAPTOP-LQHESKE6\stefa <stefano.bergamin.001@gmail.com>
2025-01-12 12:44:53 +0100

    Add HomeView

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // esbuild: {
+  //   target: 'esnext',
+  //   platform: 'linux',
+  // },
   plugins: [
     vue(),
     vueDevTools(),
